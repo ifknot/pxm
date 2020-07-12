c840b02be3d4bf48d17bf9fd216ed69293c332fc
headwedge <headwedge@hotmail.co.uk>
2020-07-12 16:40:02 +0100

    test threaded

--- test_000_spsc.cpp
+++ test_000_spsc.cpp
@@ -1,6 +1,7 @@
 #include "catch2.h"
 
 #include <iostream>
+#include <thread>
 
 #include "spsc_concurrent_bounded_queue.h"
 
@@ -16,7 +17,7 @@ SCENARIO("spsc queue", "[queues]") {
 		REQUIRE(q.was_empty());
 		REQUIRE_FALSE(q.was_full());
 		WHEN("fill queue") {
-			int i{ 0 };
+			int i{ 1 };
 			while (q.try_push(i++)) {}
 			THEN("full queue") {
 				REQUIRE(q.size_guess() == TEST_CAPACITY);
@@ -25,12 +26,12 @@ SCENARIO("spsc queue", "[queues]") {
 				REQUIRE(q.was_full());
 			}
 			WHEN("drain queue") {
-				int i, j{ 0 };
+				int i, j{ 1 };
 				while (q.try_pop(i)) {
 					REQUIRE(i == j++);
 				}
 				THEN("empty queue again") {
-					//REQUIRE(i == TEST_CAPACITY);
+					REQUIRE(i == TEST_CAPACITY);
 					REQUIRE(q.size_guess() == 0);
 					REQUIRE(q.capacity() == TEST_CAPACITY);
 					REQUIRE(q.was_empty());
@@ -38,8 +39,37 @@ SCENARIO("spsc queue", "[queues]") {
 				}
 			}
 		}
-		
+		GIVEN("producer and consumer") {
+			static int sample_size{ 400 };
+			bool stop{ false };
+			int observed_sum{ 0 };
+
+			auto producer = std::thread([&q, & stop]() {
+				for (int i{ 1 }; i <= sample_size; ++i) {
+					while (!q.try_push(i)) {}
+				}
+				stop = true;
+			});
+			
+			auto consumer = std::thread([&q, &observed_sum, &stop]() {
+				int i;
+				while (!stop || !q.was_empty()) {
+					q.try_pop(i);
+					observed_sum += i;
+				}
+			}); 
+
+			producer.join();
+			consumer.join();
+			int expected_sum = (sample_size * (sample_size + 1)) / 2;
+			REQUIRE(observed_sum == expected_sum);
+			REQUIRE(q.size_guess() == 0);
+			REQUIRE(q.capacity() == TEST_CAPACITY);
+			REQUIRE(q.was_empty());
+			REQUIRE_FALSE(q.was_full());
+		}
 	}
+	
 }
 
 /*
